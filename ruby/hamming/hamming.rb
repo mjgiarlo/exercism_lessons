491a622e8d6a50deb6bae397260197a6ec34aa5b
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-08-25 19:31:15 -0700

    Different solution for Ruby exercise 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,19 +1,9 @@
 class Hamming
   def self.compute(strand1, strand2)
-    @strand1 = strand1
-    @strand2 = strand2
-    validate!
-    pairs.reduce(0) do |memo, pair|
-      pair.uniq.length == 1 ? memo : memo + 1
+    raise ArgumentError unless strand1.length == strand2.length
+    strand1.chars.zip(strand2.chars).count do |pair|
+      pair.first != pair.last
     end
-  end
-
-  def self.pairs
-    @strand1.split('').zip(@strand2.split(''))
-  end
-
-  def self.validate!
-    raise ArgumentError if @strand1.length != @strand2.length
   end
 end
 
