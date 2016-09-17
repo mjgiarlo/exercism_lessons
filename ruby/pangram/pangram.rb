610cab88482df0466ad034bdf140ab4db2e15ebc
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-09-16 21:16:51 -0700

    Add 2nd iteration of ruby pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,13 +2,7 @@ module Pangram
   ALPHABET = ('a'..'z').to_a
 
   def self.is_pangram?(sentence)
-    ALPHABET.all? do |letter|
-      unique_characters(sentence).include?(letter)
-    end
-  end
-
-  def self.unique_characters(string)
-    string.downcase.chars.uniq
+    (ALPHABET - string.downcase.chars.uniq).empty?
   end
 end
 
